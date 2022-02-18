0c48cc65d1dd0b8d56a8c37694d578f8791eff48
milonsheikh88 <milonsheikh88@gmail.com>
2022-02-18 09:49:00 +0600

    Upload 05-Spring-Jdbc-with-XML-DaoSuurt

---
+++ 05-Spring-Jdbc-with-XML-DaoSuurt/src/main/java/com/milonsheikh/jdbc/dao/EmployeeDaoSupport.java
@@ -0,0 +1,63 @@
+package com.milonsheikh.jdbc.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Component;
+import com.milonsheikh.jdbc.model.Employee;
+
+public class EmployeeDaoSupport extends JdbcDaoSupport{
+
+	//Insert data
+	public int saveEmployee(Employee employee) {
+		String sql="insert into employee (name, salary) values('"+employee.getName()+"','"+employee.getSalary()+"')";
+		return getJdbcTemplate().update(sql);	
+	}
+	
+	//Insert data by prepared statement
+	public int saveEmployeeByPS(Employee employee) {
+		String sql = "insert into employee (name, salary) values(?,?)";
+		int r =getJdbcTemplate().update(sql,employee.getName(), employee.getSalary());
+		return r;
+	}
+	
+	//Select single row single value
+	public String getEmployeeNameById(int id) {
+		String sql = "select name from employee where id=?";
+		return getJdbcTemplate().queryForObject(sql,new Object[] {id},String.class);
+	}
+	
+	//Select single row Multiple value
+	public Employee getEmployeeAllDataById(int id) {
+		String sql = "select * from employee where id=?";
+		return getJdbcTemplate().queryForObject(sql,new Object[] {id},new EmployeeRowMapper());	
+	}
+	
+	
+	//Select Multiple row Multiple value
+	public List <Employee> getAllEmployeeAllData() {
+		String sql = "select * from employee";
+		return getJdbcTemplate().query(sql,new EmployeeRowMapper());	
+	}
+	
+	public class EmployeeRowMapper implements RowMapper<Employee>{
+
+		public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Employee employee= new Employee();
+			employee.setId(rs.getInt("id"));
+			employee.setName(rs.getString("name"));
+			employee.setSalary(rs.getDouble("salary"));
+			return employee;
+		}
+		
+		
+	}
+	
+}
