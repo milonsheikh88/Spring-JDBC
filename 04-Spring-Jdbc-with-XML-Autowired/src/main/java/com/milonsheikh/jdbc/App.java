458ad6097e325c7d08a27cbb8396b4cffadf1ae4
milonsheikh88 <milonsheikh88@gmail.com>
2022-02-18 09:48:07 +0600

    Upload 04-Spring-Jdbc-with-XML-Autowired

---
+++ 04-Spring-Jdbc-with-XML-Autowired/src/main/java/com/milonsheikh/jdbc/App.java
@@ -0,0 +1,41 @@
+package com.milonsheikh.jdbc;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.milonsheikh.jdbc.dao.EmployeeDao;
+import com.milonsheikh.jdbc.model.Employee;
+
+
+public class App {
+	public static void main(String[] args) {
+
+		//Spring jdbc ==>> JdbcTemplate
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		EmployeeDao employeeDao = context.getBean("employeeDao",EmployeeDao.class);	
+		
+		// Insert Data
+//		int result = employeeDao.saveEmployee(new Employee("Mamun Sheikh", 3000));
+//		System.out.println("Insert ==>> "+result);
+		// Insert Data by prepared statement
+//		int result = employeeDao.saveEmployeeByPS(new Employee("Amir Sheikh", 5000));
+//		System.out.println("Insert ==>> "+result);
+		
+		
+		//Select single row single value
+//		String result = employeeDao.getEmployeeNameById(1);
+//		System.out.println("getEmployeeNameById ==>> "+result);
+		
+		//Select single row Multiple value
+//		Employee emp= employeeDao.getEmployeeAllDataById(1);
+//		System.out.println("getEmployeeAllDataById ==>> "+emp);
+		
+		
+		//Select Multiple row Multiple value
+		List <Employee> allEmp= employeeDao.getAllEmployeeAllData();
+		for(Employee e:allEmp) {
+			System.out.println(e);
+		}
+	}
+}
